0d7a28aa53843b497a6b3a227cce7c703612d326
Jordi Casanella <jordi.casanella@Jordis-MBP.home>
2022-12-17 23:55:19 +0100

    First version checking user login

--- controller/login.go
+++ controller/login.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jcasanella/chat_app/model"
 )
 
 type LoginController struct{}
 
 func (lc LoginController) Status(c *gin.Context) {
-	c.String(http.StatusOK, "working!!!")
+	var user model.User
+	c.Bind(&user)
+	if user.Name != "" && user.Password != "" {
+		c.String(http.StatusOK, "Valid user")
+	} else {
+		c.String(http.StatusBadRequest, "Invalid user")
+	}
 }
